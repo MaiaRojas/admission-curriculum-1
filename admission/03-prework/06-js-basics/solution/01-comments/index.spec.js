9e65a01cea345b56f27258a96341abe0428fafea
merunga <merunga@gmail.com>
2021-04-30 21:07:19 -0500

    prework(redesign): js basics comments tests

---
+++ admission/03-prework/06-js-basics/solution/01-comments/index.spec.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const esprima = require('esprima');
+
+const script = fs.readFileSync('./index.js', 'utf-8');
+const ast = esprima.parseScript(script, { comment: true });
+
+describe('HTML Basics: Comments', () => {
+  it('Por lo menos un comentario de linea', () => {
+    const lineComments = ast.comments.filter((c) => c.type === 'Line');
+    expect(lineComments.length).toBeGreaterThan(0);
+  });
+  it('Por lo menos un comentario de bloque', () => {
+    const blockComments = ast.comments.filter((c) => c.type === 'Block');
+    expect(blockComments.length).toBeGreaterThan(0);
+  });
+})
