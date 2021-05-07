90cf98b2129186e0266505e8f55ab7c3757d3004
merunga <merunga@gmail.com>
2021-05-06 22:45:55 -0500

    prework(redesign): test utils locally js 02

--- admission/03-prework/06-js-basics/solution/02-data-types-numbers/index.spec.js
+++ admission/03-prework/06-js-basics/solution/02-data-types-numbers/index.spec.js
@@ -1,52 +1,32 @@
 const fs = require('fs');
 const esprima = require('esprima');
+const utils = require('../06-variables/utils');
 
 const script = fs.readFileSync(__dirname + '/index.js', 'utf-8');
 const ast = esprima.parseScript(script, { comment: true });
 
-const consoleLogsArgs = ast.body.reduce((accum, i) => {
-  if (
-    i.type === 'ExpressionStatement'
-    && i.expression
-    && i.expression.type === 'CallExpression'
-    && i.expression.callee
-    && i.expression.callee.object
-    && i.expression.callee.object.name === 'console'
-    && i.expression.callee.property
-    && i.expression.callee.property.name === 'log'
-  ) {
-    return [...accum, i.expression.arguments];
-  }
-  return accum;
-}, []);
+const consoleLogsArgs = utils.getAllConsoleLogLastArgs(ast);
 
-const extractBinaryOperators = (accum, arg) => {
-  if (arg.type === 'BinaryExpression') {
-    accum.push(arg.operator);
-    extractBinaryOperators(accum, arg.left)
-    extractBinaryOperators(accum, arg.right)
-  }
-  return accum;
-}
-
-const binaryOperatorsUsed = consoleLogsArgs.reduce((accum, args) => {
-  const result = [...accum];
-  extractBinaryOperators(result, args[args.length - 1]);
-  return result;
-}, []);
+const binaryOperatorsUsed = consoleLogsArgs
+  .filter((arg) => ['BinaryExpression', 'LogicalExpression'].includes(arg.type))
+  .reduce((args, arg) => (
+    [...args, ...utils.getAll(arg, 'BinaryExpression')]
+  ), [])
+  .map((arg) => arg.operator);
 
 describe('HTML Basics: Numbers', () => {
   it('Un console.log de un numero positivo', () => {
-    const positiveNumbers = consoleLogsArgs.filter((args) => typeof args[args.length - 1].value === 'number');
+    const positiveNumbers = consoleLogsArgs.filter(
+      (arg) => typeof arg.value === 'number'
+    );
     expect(positiveNumbers.length).toBeGreaterThan(0);
   });
   it('Un console.log de un numero negativo', () => {
-    const negativeNumbers = consoleLogsArgs.filter((args) => {
-      const arg = args[args.length - 1];
-      return arg.type === 'UnaryExpression'
-        && arg.operator === '-'
-        && typeof arg.argument.value === 'number';
-    });
+    const negativeNumbers = consoleLogsArgs.filter((arg) => (
+      arg.type === 'UnaryExpression'
+      && arg.operator === '-'
+      && typeof arg.argument.value === 'number'
+    ));
     expect(negativeNumbers.length).toBeGreaterThan(0);
   });
   it('Un console.log de una operación aritmética `+`', () => {
