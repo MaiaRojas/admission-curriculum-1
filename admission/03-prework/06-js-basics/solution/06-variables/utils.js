3c2c196a1a46eb8c044855fde953cada042c4fdb
merunga <merunga@gmail.com>
2021-05-06 22:10:38 -0500

    prework(redesign): test utils locally

---
+++ admission/03-prework/06-js-basics/solution/06-variables/utils.js
@@ -0,0 +1,48 @@
+const utils = require('esprima-ast-utils');
+
+const handlers = {
+  'ConsoleLogExpressionStatement': (root) => (
+    utils.filter(root, (node) => (
+      node.type === 'ExpressionStatement'
+      && node.expression
+      && node.expression.type === 'CallExpression'
+      && node.expression.callee
+      && node.expression.callee.object
+      && node.expression.callee.object.name === 'console'
+      && node.expression.callee.property
+      && node.expression.callee.property.name === 'log'
+    ))
+  ),
+}
+
+const defaultHandler = (root, nodeType) => (
+  utils.filter(root, (node) => node.type === nodeType)
+);
+
+function getAll(root, nodeType) {
+  if (!nodeType) {
+    return utils.traverse(root, (node) => node);
+  }
+  if (typeof nodeType === 'function') {
+    return utils.filter(root, nodeType);
+  }
+  let handler = handlers[nodeType];
+  if (!handler) {
+    handler = defaultHandler;
+  }
+  return handler(root, nodeType);
+}
+
+function getAllConsoleLogArgsLast(root) {
+  return getAll(root, 'ConsoleLogExpressionStatement').map(
+    (cl) => {
+      const { arguments: args } = cl.expression;
+      return args[args.length - 1];
+    }
+  );
+}
+
+module.exports = {
+  getAll,
+  getAllConsoleLogArgsLast,
+};
