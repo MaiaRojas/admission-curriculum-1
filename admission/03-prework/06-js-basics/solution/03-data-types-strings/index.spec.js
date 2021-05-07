8292d426b6f986948f426a00c27e1164e8bb9115
merunga <merunga@gmail.com>
2021-05-06 22:29:08 -0500

    prework(redesign): test utils locally js 03

--- admission/03-prework/06-js-basics/solution/03-data-types-strings/index.spec.js
+++ admission/03-prework/06-js-basics/solution/03-data-types-strings/index.spec.js
@@ -1,86 +1,41 @@
 const fs = require('fs');
 const esprima = require('esprima');
+const utils = require('../06-variables/utils');
 
 const script = fs.readFileSync(__dirname + '/index.js', 'utf-8');
 const ast = esprima.parseScript(script, { comment: true });
 
-const consoleLogsArgs = ast.body.reduce((accum, i) => {
-  if (
-    i.type === 'ExpressionStatement'
-    && i.expression
-    && i.expression.type === 'CallExpression'
-    && i.expression.callee
-    && i.expression.callee.object
-    && i.expression.callee.object.name === 'console'
-    && i.expression.callee.property
-    && i.expression.callee.property.name === 'log'
-  ) {
-    return [...accum, i.expression.arguments];
-  }
-  return accum;
-}, []);
-
-const extractUnaryOperators = (accum, arg) => {
-  if (arg && arg.type === 'UnaryExpression') {
-    accum.push(arg.operator);
-    extractUnaryOperators(arg.argument);
-  }
-  if (arg && arg.type === 'BinaryExpression') {
-    extractUnaryOperators(accum, arg.left)
-    extractUnaryOperators(accum, arg.right)
-  }
-  return accum;
-}
-
-const extractBinaryOperators = (accum, arg) => {
-  if (arg && arg.type === 'UnaryExpression') {
-    extractBinaryOperators(arg.argument);
-  }
-  if (arg && arg.type === 'BinaryExpression') {
-    accum.push(arg.operator);
-    extractBinaryOperators(accum, arg.left)
-    extractBinaryOperators(accum, arg.right)
-  }
-  return accum;
-}
-
-const unaryOperatorsUsed = consoleLogsArgs.reduce((accum, args) => {
-  const result = [...accum];
-  extractUnaryOperators(result, args[args.length - 1]);
-  return result;
-}, []);
-
-const binaryOperatorsUsed = consoleLogsArgs.reduce((accum, args) => {
-  const result = [...accum];
-  extractBinaryOperators(result, args[args.length - 1]);
-  return result;
-}, []);
+const consoleLogsArgs = utils.getAllConsoleLogLastArgs(ast);
+const consoleLogsStringArgs = consoleLogsArgs.filter((arg) => (
+  arg.value
+  && typeof arg.value === 'string'
+));
 
 describe('HTML Basics: Strings', () => {
   it('Un console.log de algún string con \'', () => {
-    const stringsDouble = consoleLogsArgs.filter((args) => {
-      const arg = args[args.length - 1];
-      return arg.value
-        && typeof arg.value === 'string'
-        && arg.raw.startsWith('\'')
-        && arg.raw.endsWith('\'');
-    });
-    expect(stringsDouble.length).toBeGreaterThan(0);
-  });
-  it('Un console.log de algún string con "', () => {
-    const stringsSingle = consoleLogsArgs.filter((args) => {
-      const arg = args[args.length - 1];
-      return arg.value
-        && typeof arg.value === 'string'
-        && arg.raw.startsWith('"')
-        && arg.raw.endsWith('"')
-    });
+    const stringsSingle = consoleLogsStringArgs.filter((arg) => (
+      arg.raw.startsWith('\'') && arg.raw.endsWith('\'')
+    ));
     expect(stringsSingle.length).toBeGreaterThan(0);
   });
+  it('Un console.log de algún string con "', () => {
+    const stringsDouble = consoleLogsStringArgs.filter((arg) => (
+      arg.raw.startsWith('"') && arg.raw.endsWith('"')
+    ));
+    expect(stringsDouble.length).toBeGreaterThan(0);
+  });
   it('Un console.log de alguna concatenación', () => {
+    const binaryOperatorsUsed = consoleLogsArgs
+      .filter((arg) => ['BinaryExpression', 'LogicalExpression'].includes(arg.type))
+      .map((arg) => arg.operator);
+
     expect(binaryOperatorsUsed).toEqual(expect.arrayContaining(['+', '+']));
   });
   it('Un console.log con typeof', () => {
+    const unaryOperatorsUsed = consoleLogsArgs
+      .filter((arg) => arg.type === 'UnaryExpression')
+      .map((arg => arg.operator));
+  
     expect(unaryOperatorsUsed).toContain('typeof');
   });
 });
