7802f621e6e3c76773546da33283f80de417a19b
merunga <merunga@gmail.com>
2021-05-14 07:59:42 -0500

    prework(redesign): copiar tests y package.json dentro de los sandbox

---
+++ admission/03-prework/06-js-basics/sandbox/04-data-types-booleans/test/index.spec.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const esprima = require('esprima');
+const { esprima: utils } = require('@laboratoria/prework-test-utils');
+
+const script = fs.readFileSync(__dirname + '/../src/index.js', 'utf-8');
+const ast = esprima.parseScript(script);
+
+const consoleLogsArgs = utils.getAllConsoleLogLastArgs(ast);
+
+const binaryOperatorsUsed = utils.getNestedBinaryExpressions(consoleLogsArgs)
+  .map((arg) => arg.operator);
+
+describe('JS Basics: Booleans', () => {
+  it('Un console.log de comparación <', () => {
+    expect(binaryOperatorsUsed).toContain('<');
+  });
+  it('Un console.log de comparación <', () => {
+    expect(binaryOperatorsUsed).toContain('>');
+  });
+  it('Un console.log de comparación ===', () => {
+    expect(binaryOperatorsUsed).toContain('===');
+  });
+  it('Un console.log de comparación !==', () => {
+    expect(binaryOperatorsUsed).toContain('!==');
+  });
+  it('Un typeof combinado con comparación', () => {
+    const typeofBoolean = utils.getNestedUnaryExpressions(consoleLogsArgs)
+      .filter((arg) => (
+        arg.operator === 'typeof'
+        && arg.argument.type === "BinaryExpression"
+        && ['>', '<'].includes(arg.argument.operator)
+      ));
+
+    expect(typeofBoolean.length).toBeGreaterThan(0);
+  });
+  it('Un console.log de operaciones booleanas &&', () => {
+    expect(binaryOperatorsUsed).toContain('&&');
+  });
+  it('Un console.log de operaciones booleanas ||', () => {
+    expect(binaryOperatorsUsed).toContain('||');
+  });
+  it('Un console.log de operaciones booleanas !', () => {
+    const unaryOperatorsUsed = consoleLogsArgs
+      .filter((arg) => arg.type === 'UnaryExpression')
+      .map((arg => arg.operator));
+
+    expect(unaryOperatorsUsed).toContain('!');
+  });
+  it('Un console.log de operador ternario ?:', () => {
+    const ternaryOperatorsUsed = utils
+      .getNestedExpressions(consoleLogsArgs, 'ConditionalExpression');
+
+    expect(ternaryOperatorsUsed.length).toBeGreaterThan(0);
+  });
+});
---
+++ scripts/copy-tests-to-sandboxes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for d in ./admission/03-prework/*; do
+  if [ -d "$d" ]; then
+    for sd in $d/solution/*; do
+      if [ -d "$sd" ]; then
+        target=${sd/solution/sandbox}
+        cp -rf "$sd/package.json" "$sd/package-lock.json" "$sd/test" $target
+      fi
+    done
+  fi
+done
