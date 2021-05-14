fdf144f6eab2157c00b6a6ba339cb42b2a3e6f43
Monica <monicasayama@gmail.com>
2021-05-14 18:36:04 -0300

    add basic functions tests

---
+++ admission/03-prework/06-js-basics/solution/08-dom-api-basics/test/index.spec.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const esprima = require('esprima');
+const { esprima: utils } = require('@laboratoria/prework-test-utils');
+
+const script = fs.readFileSync(__dirname + '/../src/index.js', 'utf-8');
+const astLocs = esprima.parseScript(script, { loc: true });
+const ast = esprima.parseScript(script);
+
+const functionExpressions = utils.getAll(ast.body, 'FunctionExpression');
+
+describe('JS Basics: DOM', () => {
+  it('Al menos un elemento con id', () => {
+  });
+  it('Al menos un elemento con class', () => {
+  });
+  it('Al menos un elemento sin id ni class y con la misma etiqueta q el class de arriba', () => {
+  });
+  it('Un getElementById con un id q exista', () => {
+  });
+  it('Un querySelector con un selector que exista', () => {
+  });
+  it('Una definición de una function', () => {
+  });
+  it('Un addEventListener sobre un elemento existente, un evento valido y un callback existente', () => {
+  });
+});
