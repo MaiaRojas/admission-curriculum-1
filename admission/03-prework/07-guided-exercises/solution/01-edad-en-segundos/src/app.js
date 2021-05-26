51b70c15cd1100777abfc928c6881d7e16c9f381
merunga <merunga@gmail.com>
2021-05-25 19:59:18 -0500

    prework(redesign): solution y tests para edad en segundos

---
+++ admission/03-prework/07-guided-exercises/solution/01-edad-en-segundos/src/app.js
@@ -0,0 +1,5 @@
+const edad = prompt('¿Cuál es tu edad?');
+
+const edadEnSegundos = edad * 365 * 24 * 60 * 60;
+
+document.write("Tu edad es " + edad + ", en segundos sería " + edadEnSegundos);
---
+++ admission/03-prework/07-guided-exercises/solution/01-edad-en-segundos/test/index.spec.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const { e2e: { initStaticServer, stopStaticServer } } = require('@laboratoria/prework-test-utils');
+
+const srcPath = path.normalize(__dirname + '/../src');
+let server;
+
+const testConversion = async ({ page, done, anios, segundos }) => {
+  page.on('dialog', (dialog) => {
+    dialog.accept(`${anios}`);
+  });
+  await page.goto('http://localhost:5000');
+  const message = await page.$eval('body', (el) => Array.prototype.filter
+    .call(el.childNodes, (child, index) => index === 0 && child.nodeType === Node.TEXT_NODE)
+    .map((child) => child.textContent)
+  );
+  expect(message.length).toBeGreaterThan(0);
+  expect(message[0].trim().replace(/\n/ig, '')).toBe(`Tu edad es ${anios}, en segundos sería ${segundos}`);
+  done();
+}
+
+describe('Guided Exercises: Edad en segundos', () => {
+  beforeAll((done) => {
+    server = initStaticServer(srcPath, {}, done);
+  });
+
+  afterEach(async () => {
+    page = await browser.newPage();
+  });
+
+  it('La página tiene el title correcto', async (done) => {
+    page.on('dialog', (dialog) => {
+      dialog.accept('');
+    });
+    await page.goto('http://localhost:5000');
+    expect(page.title()).resolves.toMatch('Edad en segundos').then(done);
+  });
+
+  it('El prompt muestra el mensaje correcto', async (done) => {
+    let message;
+    page.on('dialog', (dialog) => {
+      message = dialog.message();
+      dialog.accept('');
+    });
+    await page.goto('http://localhost:5000');
+    expect(message).toBe('¿Cuál es tu edad?');
+    done();
+  });
+
+  it('20 Años === 630720000 segundos', async (done) => {
+    const anios = 20;
+    const segundos = 630720000;
+    await testConversion({
+      page,
+      done,
+      anios,
+      segundos,
+    })
+  });
+
+  it('0 Años === 0 segundos', async (done) => {
+    const anios = 0;
+    const segundos = 0;
+    await testConversion({
+      page,
+      done,
+      anios,
+      segundos,
+    })
+  });
+
+  afterAll((done) => {
+    stopStaticServer(server, done);
+  });
+});
